8bc0b46ccbc9a77d41ea3745f7c9cac62e1f0d17
Sahil Shende <ssahilshende@gmail.com>
2021-07-24 22:44:32 +0530

    connect mongodb and add new-item function

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const date = require(__dirname+"/date.js");
 console.log(date);
 const PORT = 3000;
 const app = express();
-let homeList = [];
-let workList = [];
+// let homeList = [];
+// let workList = [];
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
 
 app.set('view engine', 'ejs');
 // Body Parser - Express v4.16.0 and higher
@@ -15,13 +18,51 @@ app.use(express.urlencoded({
 // For static files css, images, etc
 app.use(express.static("public"));
 
+const itemsSchema = new mongoose.Schema({
+    name : String
+})
+
+const homeItem = mongoose.model('homeItem', itemsSchema);
+
+/* Create 3 default documents */
+const default1 = new homeItem({name : "Welcome to your to-do list"});
+const default2 = new homeItem({name : "Hit the + button to add new item"});
+const default3 = new homeItem({name : "<-- hit this to delete the item"});
+const defArr = [default1, default2, default3];
+
+
 app.get("/", (req, res) => {
     // get today's date string
     
     let today = date.getDate();
+    
 
-    res.render('list', {listTitle : today, newItems: homeList});
-})
+    homeItem.find({}, function(err, docs){
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            if(docs.length === 0)
+            {
+                /* Add default docs to homeItems collection : C for CREATE/INSERT */
+                homeItem.insertMany(defArr,function(err){
+                    if(err){
+                        console.log(err);
+                    }else{
+                        console.log("inserted defaults successfully");
+                    }
+                });
+                res.redirect("/");
+            }
+            else{
+                res.render('list', {listTitle : today, newItems: docs});
+            }
+        }
+    });
+
+});
 
 app.get("/work", (req, res) =>{
     res.render('list', {listTitle:"Work", newItems: workList})
@@ -30,7 +71,7 @@ app.get("/work", (req, res) =>{
 app.post("/", (req, res)=>{
 
     let listPlace = req.body.submitButton;
-    let item = req.body.newItem;
+    let itemName = req.body.newItem;
     console.log(listPlace);
     if(listPlace === "Work")
     {
@@ -39,7 +80,10 @@ app.post("/", (req, res)=>{
     }
     else
     {   
-        homeList.push(item); 
+        const newItem = new homeItem({
+            name : itemName
+        });
+        newItem.save(); 
         res.redirect("/");
     }
 })
