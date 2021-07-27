326f22e71eb293d94b6fed6a7bcd43096e37b03e
Sahil Shende <ssahilshende@gmail.com>
2021-07-27 14:18:59 +0530

    v1

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 // let workList = [];
 const mongoose = require("mongoose");
 const { addListener } = require('nodemon');
-mongoose.connect('mongodb+srv://admin-sahil:Sahil@2001@cluster0.tyon0.mongodb.net/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://admin-sahil:---Password---@cluster0.tyon0.mongodb.net/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const _ = require("lodash");
 
